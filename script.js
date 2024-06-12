844c5edacfdc65fa1931e1fa5f5311b9775023c8
poopy2121 <ggg1111g1g1g@gmail.com>
2024-06-12 18:00:58 +0200

    poop commit

--- script.js
+++ script.js
@@ -2,25 +2,26 @@ const h1 = document.querySelector(".hi")
 const arrowup = document.createElement('img')
 const imgurl = 'iamges/arrowup.jpg'
 arrowup.src = imgurl
-let user
 const gohigher = new Audio('soundeffects/ding-126626.mp3');
 const golower = new Audio('soundeffects/boing-2-44164.mp3');
 const won = new Audio('soundeffects/yay-6120.mp3');
 let username
+let user
+
 
 function start() {
 
     let correct = Math.floor(Math.random() * 9 + 1);
-        let username = prompt("ur name?")
+         username = prompt("ur name?")
 
 addEventListener('keydown', function(event) {
     console.log(username);
     
     if (event.key === 'Enter') {
         console.log(correct);
-       
-        let user = prompt("pls enter a number" + username)
-               
+
+         user = prompt("pls enter a number" + username);
+
         if (correct > user) {           
              gohigher.play();           
             alert("higher");
@@ -36,23 +37,23 @@ addEventListener('keydown', function(event) {
         else {
             won.play();
             alert("congrats");
+            arrowup.remove();
             correct = Math.floor(Math.random() * 9 + 1); 
             alert("a new number has been chosen")
         }
 
         if (username === null) {
             alert("u gotta enter a name");
-
         }
+    }
+
+    while (true) {
+        arrowup.remove();
+        
     }
 });
 }
-
 start();
-  
-    
-    
-
 // if i put math floor within the eventlistener, each time enter i pressed a new number is calculated, which is why i have to declare it outside 
 
   
