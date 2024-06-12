4160417d2b19e015c0b6f9aeef12d1e9f45adf6e
poopy2121 <ggg1111g1g1g@gmail.com>
2024-06-12 15:58:43 +0200

    passt  soweit, jetzt sound efeects

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let user
-let correct = Math.floor(Math.random() * 20);
+const gohigher = new Audio();
+const golower = new Audio();
 
+
+function start() {
+    let correct = Math.floor(Math.random() * 3);
 
 addEventListener('keydown', function(event) {
+    
     if (event.key === 'Enter') {
        
         let user = prompt("pls enter a number")
                
         if (correct > user) {
-            alert("go higher");
+            alert("higher");
             
         }
 
@@ -16,21 +21,23 @@ addEventListener('keydown', function(event) {
             alert("lower");
         }
 
+        
+        // user guesses correctly
+        
         else {
-
-
             alert("congrats");
-            
-            
+            correct = Math.floor(Math.random() * 3); 
+            alert("a new number has been chosen")
         }
 
     }
-    
-})
+});
+}
 
-            
-        
-
+        start();
     
+    
+
+// if i put math floor within the eventlistener, each time enter i pressed a new number is calculated, which is why i have to declare it outside 
 
   
