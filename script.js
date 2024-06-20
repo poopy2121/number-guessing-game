a363909a887513265a81a4abf8d5b0c6e7652a44
poopy2121 <ggg1111g1g1g@gmail.com>
2024-06-20 19:02:13 +0200

    score system (angefangen)

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const h1 = document.querySelector(".hi")
 const arrowup = document.createElement('img')
 const imgurl = 'iamges/arrowup.jpg'
 arrowup.src = imgurl
+const arrowdown = document.createElement('img')
+const arrowdownurl = 'iamges/arrowdown.jpg'
+arrowdown.src = arrowdownurl
 const gohigher = new Audio('soundeffects/ding-126626.mp3');
 const golower = new Audio('soundeffects/boing-2-44164.mp3');
 const won = new Audio('soundeffects/yay-6120.mp3');
 let username
 let user
+let score = 0;
+
+
+
 function start() {
 
     let correct = Math.floor(Math.random() * 9 + 1);
@@ -17,27 +24,36 @@ addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         const userbox = document.getElementById("n")
         const button = document.getElementById("btn");
+        let sc = document.getElementById("sc")
+        alert("the game starts"); 
 
 
 
         button.addEventListener('click', function() {
-            console.log(user);
 
 
          user = userbox.value
+        console.log(user);
+
+
+
         
 
         if (correct > user) {           
              gohigher.play();           
             alert("higher");
             h1.append(arrowup);
+            arrowdown.remove();
+            score += 5;
         }
 
-         
 
         else if (correct < user) {
             golower.play();
             alert("lower");
+            h1.append(arrowdown);
+            arrowup.remove();
+            score +=5;
         }
 
         
@@ -47,9 +63,11 @@ addEventListener('keydown', function(event) {
             won.play();
             alert("congrats");
             arrowup.remove();
+            arrowdown.remove();
             correct = Math.floor(Math.random() * 9 + 1); 
             alert("a new number has been chosen")
         }
+        sc.innerText = score;
 
         
      })
